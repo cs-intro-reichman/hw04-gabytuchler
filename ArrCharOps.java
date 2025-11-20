f98438b54aeacb9871de29ce12fca58cfac4c7c7
gaby tuchler <gabytuchler@MacBook-Air-sl-gaby.local>
2025-11-21 01:52:48 +0200

    C

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -154,13 +154,14 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        str1=str1.toLowerCase();
-        str2=str2.toLowerCase();
+        
 
         if (str1 == null || str2 == null) {
             return -2;}
 
         for (int i=0; i<Math.min(str1.length(), str2.length()); i++) {
+            str1=str1.toLowerCase();
+            str2=str2.toLowerCase();
             if (str1.charAt(i) < str2.charAt(i)) {
                 return -1;}
             else if (str1.charAt(i) > str2.charAt(i)) {
