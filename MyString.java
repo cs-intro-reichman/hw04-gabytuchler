585db2c3bd6aff9c25879862b0cd28e02f56990b
gaby tuchler <gabytuchler@MacBook-Air-sl-gaby.local>
2025-11-20 18:31:07 +0200

    c

--- MyString.java
+++ MyString.java
@@ -30,12 +30,19 @@ public class MyString {
 
 
     /** If str1 contains str2, returns true; otherwise returns false. */
-    public static boolean contains(String str1, String str2) {}
-        //boolean contains = true;
-        //if (str2.length() > str1.length()) {
-          //  return false;}
-        //for (int i = 0; i <= str1.length(); i++) {
-        //}  
-        //return false;
+    public static boolean contains(String str1, String str2) {
+        if (str2.length() > str1.length()) {
+            return false;}
+        for (int i = 0; i <(str1.length()-str2.length()); i++) {
+            boolean match = true;
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i+j) != str2.charAt(j)) {
+                    match = false;
+                    break;}}
+        
+        if (match) {
+            return true;}}
+        return false;
     
-
+        }
+    }
