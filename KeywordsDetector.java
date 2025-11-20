ce07289275b91fa5e55682a28dbaec15fa2de2be
gaby tuchler <gabytuchler@MacBook-Air-sl-gaby.local>
2025-11-20 18:55:12 +0200

    c

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,16 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        
+
+        for (int i=0; i<(sentences.length); i++){
+            String ch1 = sentences[i].toLowerCase();
+            for (int j = 0; j < keywords.length ; j++) {
+                String ch2 = keywords[j].toLowerCase();
+                if (ch1.contains(ch2)){
+                    System.err.println(ch1);}
+                }
     }
 }
+}
+
