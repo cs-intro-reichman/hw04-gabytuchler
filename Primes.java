691ac50f4df23f84096a34e4250ab455f3c17958
gaby tuchler <gabytuchler@MacBook-Air-sl-gaby.local>
2025-11-21 01:36:13 +0200

    m

--- Primes.java
+++ Primes.java
@@ -1,19 +1,27 @@
 public class Primes {
     public static void main(String[] args) {
-        printPrimesUpTo(30); 
+    if (args.length == 0) {
+        return; 
     }
+    int n = Integer.parseInt(args[0]); 
+    printPrimesUpTo(n); 
+}
 
     public static void printPrimesUpTo(int n) {
         if (n < 2) {
             System.out.println("אין מספרים ראשוניים.");
             return;
-        }                
+        }
+
+        System.out.println("Prime numbers up to " + n + ":");
+        int primeCount = 0;
         for (int i = 2; i <= n; i++) {
             if (isPrime(i)) {
-                System.out.print(i + " ");
+                System.out.println(i);
+                primeCount++;
             }
         }
-        System.out.println(); 
+         System.out.println("There are " + primeCount + " primes...");
     }
     
     public static boolean isPrime(int number) {
